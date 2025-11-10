7d9b431be1835371ab5e7ee173f6b4030069dce7
Rick Mouritzen <rmouritzen@splunk.com>
2025-11-09 20:13:37 -0800

    Change version to 4.0.0-beta.1

--- mix.exs
+++ mix.exs
@@ -10,7 +10,7 @@
 defmodule Schema.MixProject do
   use Mix.Project
 
-  @version "4.0.0-beta"
+  @version "4.0.0-beta.1"
 
   def project do
     build = System.get_env("GITHUB_RUN_NUMBER") || "SNAPSHOT"
